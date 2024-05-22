3b7f1c6155cc02d0cab04eba4373d818273cf201
Aatif Syed <aatifsyedyp@gmail.com>
2024-05-23 01:15:42 +0200

    chore: prepare

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,3 +1,41 @@
-fn main() -> anyhow::Result<()> {
-    Ok(())
+use itertools::Itertools as _;
+use pulldown_cmark::{BrokenLink, Event, Options, Tag, TagEnd};
+
+const SPEC_MD: &str = include_str!("../spec.md");
+
+fn main() {
+    let mut events = pulldown_cmark::Parser::new_with_broken_link_callback(
+        SPEC_MD,
+        Options::all(),
+        Some(|BrokenLink { reference, .. }| Some((reference.clone(), reference))),
+    )
+    .peekable();
+    while let Some(it) = events.next() {
+        if let Event::Start(Tag::Heading { .. }) = it {
+            let Some(Event::Text(title)) = events.next() else {
+                panic!()
+            };
+            if let Some(obj) = title.strip_suffix("Object").map(str::trim) {
+                dbg!(obj);
+                events
+                    .find(|it| matches!(it, Event::Start(Tag::Table(_))))
+                    .unwrap();
+                let table = events
+                    .peeking_take_while(|it| !matches!(it, Event::End(TagEnd::Table)))
+                    .collect::<Vec<_>>();
+                dbg!(table);
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    for event in pulldown_cmark::Parser::new_with_broken_link_callback(
+        SPEC_MD,
+        Options::all(),
+        Some(|BrokenLink { reference, .. }| Some((reference.clone(), reference))),
+    ) {
+        println!("{:?}", event)
+    }
 }
