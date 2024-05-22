897b9ab83d0dbad0539a741a32a01ead518f0ef8
Aatif Syed <aatifsyedyp@gmail.com>
2024-05-22 16:08:48 +0200

    refactor!: remove validated vecs

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,11 @@
 //!
 //! > When quoted, the specification will appear as blockquoted text, like so.
 
-use itertools::Itertools as _;
 use schemars::schema::Schema;
 use semver::Version;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::BTreeMap;
-use thiserror::Error;
 use url::Url;
 
 /// > This is the root object of the OpenRPC document.
@@ -28,7 +26,7 @@ pub struct OpenRPC {
     /// > REQUIRED.
     /// > The available methods for the API.
     /// > While it is required, the array may be empty (to handle security filtering, for example).
-    pub methods: Methods,
+    pub methods: Vec<Method>,
     /// > An element to hold various schemas for the specification.
     #[serde(default)]
     pub components: Components,
@@ -113,7 +111,12 @@ pub struct Method {
     pub description: Option<String>,
     /// > Additional external documentation for this method.
     pub external_docs: Option<ExternalDocumentation>,
-    pub params: Params,
+    /// > REQUIRED.
+    /// > A list of parameters that are applicable for this method.
+    /// > The list MUST NOT include duplicated parameters and therefore require name to be unique.
+    /// > The list can use the Reference Object to link to parameters that are defined by the Content Descriptor Object.
+    /// > All optional params (content descriptor objects with “required”: false) MUST be positioned after all required params in the list.
+    pub params: Vec<ContentDescriptor>,
     /// > The description of the result returned by the method.
     /// > If defined, it MUST be a Content Descriptor or Reference Object.
     /// > If undefined, the method MUST only be used as a notification.
@@ -298,36 +301,6 @@ impl<'de> Deserialize<'de> for SpecificationExtensions {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Default)]
-#[serde(transparent)]
-pub struct Methods {
-    inner: Vec<Method>,
-}
-
-#[derive(Debug, Clone, Error)]
-#[error("{}", .0)]
-pub struct MethodsError(String);
-
-impl Methods {
-    pub fn new(methods: impl IntoIterator<Item = Method>) -> Result<Self, MethodsError> {
-        let inner = methods.into_iter().collect::<Vec<_>>();
-        let duplicates = inner
-            .iter()
-            .map(|it| &it.name)
-            .duplicates()
-            .collect::<Vec<_>>();
-        match duplicates.is_empty() {
-            true => Ok(Self { inner }),
-            false => Err(MethodsError(format!(
-                "the following method names are duplicated: [{}]",
-                duplicates.iter().join(", ")
-            ))),
-        }
-    }
-}
-
-validated_vec!(Methods => Method);
-
 /// > The expected format of the parameters.
 /// > As per the JSON-RPC 2.0 specification,
 /// > the params of a JSON-RPC request object may be an array, object, or either
@@ -345,118 +318,7 @@ pub enum ParamStructure {
     Either,
 }
 
-/// > REQUIRED.
-/// > A list of parameters that are applicable for this method.
-/// > The list MUST NOT include duplicated parameters and therefore require name to be unique.
-/// > The list can use the Reference Object to link to parameters that are defined by the Content Descriptor Object.
-/// > All optional params (content descriptor objects with “required”: false) MUST be positioned after all required params in the list.
-#[derive(Debug, Clone, PartialEq, Serialize, Default)]
-#[serde(transparent)]
-pub struct Params {
-    inner: Vec<ContentDescriptor>,
-}
-
-#[derive(Debug, Clone, Error)]
-#[error("{}", .0)]
-pub struct ParamListError(String);
-
-impl Params {
-    pub fn new(
-        params: impl IntoIterator<Item = ContentDescriptor>,
-    ) -> Result<Self, ParamListError> {
-        let params = params.into_iter().collect::<Vec<_>>();
-        let duplicates = params
-            .iter()
-            .map(|it| it.name.as_str())
-            .duplicates()
-            .collect::<Vec<_>>();
-        if !duplicates.is_empty() {
-            return Err(ParamListError(format!(
-                "The following parameter names are duplicated: [{}]",
-                duplicates.join(", ")
-            )));
-        }
-        if let Some((first_opt_ix, first_opt_param)) = params
-            .iter()
-            .enumerate()
-            .find(|(_, it)| !it.required.unwrap_or_default())
-        {
-            let late_mandatory_params = params
-                .iter()
-                .enumerate()
-                .filter(|(ix, it)| it.required.unwrap_or_default() && *ix > first_opt_ix)
-                .map(|(_, it)| it.name.clone())
-                .collect::<Vec<_>>();
-
-            if !late_mandatory_params.is_empty() {
-                return Err(ParamListError(format!(
-                    "Mandatory parameters may not follow optional parameters, \
-                            but the optional parameter {} is followed by [{}]",
-                    first_opt_param.name,
-                    late_mandatory_params.join(", ")
-                )));
-            }
-        };
-        Ok(Self { inner: params })
-    }
-}
-
-validated_vec!(Params => ContentDescriptor);
-
-/// `parent` acts as a `Vec<$child>`, with some internal invariant guarded by
-/// `$parent::new()`.
-///
-/// So you only need to implement `new`, and get `len`, `iter`, `deserialize` for free.
-macro_rules! validated_vec {
-    ($parent:ty => $child:ty) => {
-        impl $parent {
-            pub fn empty() -> Self {
-                Self::default()
-            }
-            pub fn just(one: $child) -> Self {
-                Self { inner: vec![one] }
-            }
-            pub fn len(&self) -> usize {
-                self.inner.len()
-            }
-            pub fn is_empty(&self) -> bool {
-                self.inner.is_empty()
-            }
-            pub fn iter(&self) -> std::slice::Iter<'_, $child> {
-                self.inner.iter()
-            }
-        }
-
-        impl IntoIterator for $parent {
-            type Item = $child;
-            type IntoIter = std::vec::IntoIter<$child>;
-            fn into_iter(self) -> Self::IntoIter {
-                self.inner.into_iter()
-            }
-        }
-
-        impl<'a> IntoIterator for &'a $parent {
-            type Item = &'a $child;
-            type IntoIter = std::slice::Iter<'a, $child>;
-            fn into_iter(self) -> Self::IntoIter {
-                self.inner.iter()
-            }
-        }
-
-        impl<'de> Deserialize<'de> for $parent {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-            where
-                D: serde::Deserializer<'de>,
-            {
-                Self::new(Vec::deserialize(deserializer)?).map_err(serde::de::Error::custom)
-            }
-        }
-    };
-}
-
-pub(crate) use validated_vec;
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[derive(Serialize, Deserialize)]
 #[serde(untagged, expecting = "a reference or an item")]
 enum _ReferenceOr<T> {
     Reference {
